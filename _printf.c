ff6dc6cff3bb49ca3ceb012ab03aa6f4479538bb
Yasmin So <yasmin@yasmin.so>
2026-08-04 14:04:13 +1000

    feat: handling converstion specifier

--- _printf.c
+++ _printf.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdarg.h>
 
 /**
  * _printf - produces output according to a format
@@ -10,21 +11,46 @@
 int _printf(const char *format, ...)
 {
 	int i;
-	int specifier_count;
+	int print_count;
+	char *word;
+	va_list args;
+
+	va_start(args, format);
 
 	i = 0;
-	specifier_count = 0;
+	print_count = 0;
 	while (format[i] != '\0')
 	{
 		if (format[i] == '%')
 		{
-			specifier_count = specifier_count + 1;
+			if (format[i + 1] == 'c')
+			{
+				_putchar(va_arg(args, int));
+				print_count = print_count + 1;
+				i = i + 2;
+				continue;
+			}
+			else if (format[i + 1] == 's')
+			{
+				word = va_arg(args, char*);
+				while (*word != '\0')
+				{
+					_putchar(*word);
+					word++;
+					print_count = print_count + 1;
+				}
+				i = i + 2;
+				continue;
+			}
 		}
 		_putchar(format[i]);
+		print_count = print_count + 1;
 		i = i + 1;
 	}
-	
+
+	va_end(args);
+
 	_putchar('\n');
-	_putchar(specifier_count + '0');
+	printf("%d \n", print_count);
 	return (i);
 }
