bd30172a7c3bfbabdd8de177734037989021e4d9
Yasmin So <yasmin@yasmin.so>
2026-08-04 12:23:55 +1000

    feat: adding specifier check count

--- _printf.c
+++ _printf.c
@@ -10,14 +10,21 @@
 int _printf(const char *format, ...)
 {
 	int i;
+	int specifier_count;
 
 	i = 0;
+	specifier_count = 0;
 	while (format[i] != '\0')
 	{
+		if (format[i] == '%')
+		{
+			specifier_count = specifier_count + 1;
+		}
 		_putchar(format[i]);
 		i = i + 1;
 	}
 	
 	_putchar('\n');
-	return (0);
+	_putchar(specifier_count + '0');
+	return (i);
 }
